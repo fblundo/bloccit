af1e701c681c985247a4f8ef502465b41f5e0075
fblundo <federico.blundo@gmail.com>
2019-11-04 00:08:47 +0100

    assignment intro to expres completed review

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
+const staticController = require("../controllers/staticController");
+router.get("/", staticController.index);
+
+//router.get("/", (req, res, next) => {
+//  res.send("Welcome to Bloccit");
+// });
 
 router.get("/marco", (req, res, next) => {
-  res.send("Polo");
+  res.send("Polo"); 
 });
 
 module.exports = router;
